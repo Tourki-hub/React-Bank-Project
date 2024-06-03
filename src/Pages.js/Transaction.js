a1766ce1f96bb89503478096384729e2c5570a38
Tourki-hub <tourkimufarej@gmail.com>
2024-06-03 20:48:18 +0300

    Done

--- src/Pages.js/Transaction.js
+++ src/Pages.js/Transaction.js
@@ -23,26 +23,19 @@ const Transaction = () => {
         <div className=" w-full gap-4 flex justify-center items-center">
           <div className="font-semibold">Filter:</div>
           <input
-            type="checkbox"
+            type="radio"
+            name="filter"
             defaultChecked
             className="checkbox checkbox-xs"
           />
           All
-          <input
-            type="checkbox"
-            defaultChecked
-            className="checkbox checkbox-xs"
-          />
+          <input type="radio" name="filter" className="checkbox checkbox-xs" />
           Deposit
-          <input
-            type="checkbox"
-            defaultChecked
-            className="checkbox checkbox-xs"
-          />
+          <input type="radio" name="filter" className="checkbox checkbox-xs" />
           Withdraw
           <input
             type="checkbox"
-            defaultChecked
+            name="filter"
             className="checkbox checkbox-xs"
           />
           Transfare
@@ -53,12 +46,18 @@ const Transaction = () => {
           <div className="w-full flex flex-col items-center justify-center ">
             {transactions?.map((transact) => {
               return (
-                <div key={transact._id}>
-                  {transact.type}
-                  {transact.amount}
-
-                  {transact.createdAt}
-                  {transact.updatedAt}
+                <div
+                  className="flex justify-center items-center py-6 px-5"
+                  key={transact._id}
+                >
+                  <div className="card w-200 bg-base-100 shadow-xl flex ">
+                    <tr key={transact._id}>
+                      <td className="border px-5 py-3">{transact.type}</td>
+                      <td className="border px-4 py-2">{transact.amount}</td>
+                      <td className="border px-4 py-2">{transact.createdAt}</td>
+                      <td className="border px-4 py-2">{transact.updatedAt}</td>
+                    </tr>
+                  </div>
                 </div>
               );
             })}
