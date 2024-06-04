ebcc25a1f0bfe8b80334dcb26e0f3dce80c0f43c
Tourki-hub <tourkimufarej@gmail.com>
2024-06-04 11:20:23 +0300

    update

--- src/Pages.js/Users.js
+++ src/Pages.js/Users.js
@@ -1,33 +1,46 @@
 import React from "react";
 import { getAllUsers } from "../api/auth";
 import { useQuery } from "@tanstack/react-query";
+import NavBar from "../Components/NavBar";
 
 const Users = () => {
-  const { data: users } = useQuery({
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ["users"],
     queryFn: getAllUsers,
   });
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error Occurred: {error.message}</div>;
   return (
-    <div className="bg-gray-900 min-h-screen h-screen flex items-center justify-center absolute inset-0 z-[-1]">
-      <div className="max-w-[90%] overflow-scroll w-full px-6 py-8 bg-gray-800 rounded-md shadow-md max-h-[80%]">
-        <h2 className="text-3xl text-white font-semibold mb-6 ">Users</h2>
+    <div className="bg-gray-900 min-h-screen h-screen flex items-center justify-center flex-col">
+      <div className="w-full">
+        <NavBar />
+      </div>
+
+      <div className="max-w-[90%] overflow-scroll w-full px-6 py-8 bg-[#F7F8F1] rounded-md shadow-md max-h-[90%]">
+        <h2 className="text-3xl text-black font-semibold mb-6 ">Users</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
           {users?.map((user) => (
             <div
-              key={user.id}
-              className="bg-gray-700 p-6 rounded-md flex flex-col items-center justify-center"
+              key={user._id}
+              className="bg-[#F7F8F1] p-6 rounded-md flex flex-col items-center justify-center"
             >
               <img
-                src={user.image}
+                src={
+                  "https://react-bank-project.eapi.joincoded.com/" + user?.image
+                }
                 alt="User"
                 className="w-24 h-24 rounded-full mb-4"
               />
               <div className="text-center">
-                <h3 className="text-lg text-white font-semibold mb-2">
-                  {user.name}
+                <h3 className="text-lg text-black font-semibold mb-2">
+                  {user.username}
                 </h3>
-                <p className="text-gray-300">{user.email}</p>
+                <p className="text-gray-700">{user.balance}</p>
               </div>
             </div>
           ))}
